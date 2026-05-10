ea4fe41d02932a90f29efd920d6dee695c099176
leonardocaldas <leonardo.ferreira.caldas@gmail.com>
2026-05-10 00:34:39 -0300

    feat(site): remove páginas B2C, configura 301 redirects para home

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      { source: '/area-de-atendimento', destination: '/', permanent: true },
+      { source: '/regras-programa-renda-popgas', destination: '/', permanent: true },
+      {
+        source: '/termos-de-uso-e-politica-de-privacidade-entregadores',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/politica-de-privacidade-e-autorizacao-para-tratamento-de-dados-popgas-entregadores',
+        destination: '/',
+        permanent: true,
+      },
+      { source: '/termos-de-uso', destination: '/termos', permanent: true },
+    ];
+  },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "popgas.com.br",
-      },
-      {
-        protocol: "https",
-        hostname: "*.wp.com",
-      },
-    ],
+    formats: ['image/avif', 'image/webp'],
   },
 };
 
